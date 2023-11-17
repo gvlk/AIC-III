2712a5ad9c6741bfa0a5a7d3bb9feba15911494d
Gui <88752991+gvlk@users.noreply.github.com>
2023-11-17 20:46:07 -0300

    js

---
+++ orders.js
@@ -0,0 +1,23 @@
+/*
+const Orders = [
+    {
+        productName: 'Salário',
+        productNumber: '8.5743,00',
+        paymentStatus: 'Paid',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
+
+*/
