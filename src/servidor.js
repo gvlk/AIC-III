4d366ee9b00c33c7754de67b56f3d9bcc192fc91
Ayron <ayronlouzada19@gmail.com>
2023-11-20 11:50:43 -0300

    servidor finalizado 👌

--- src/servidor.js
+++ src/servidor.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const boduParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+//CONTROLLERS
+const Logar = require('./Controllers/Users/Logar');
+const Logado = require('./Controllers/Users/Logado');
+const Deslogar = require('./Controllers/Users/Deslogar');
+
 //APP
 const app = express();
 app.use(cors());
@@ -16,6 +21,14 @@ const Database = require('./Database');
 //PAGES
 app.get('/', (req, res) => res.sendFile(__dirname+'/Pages/Index/index.html'));
 app.get('/privado', (req, res) => res.send('Somente usuarios logados podem ver isso'))
+
+//ROUTES
+app.post('/api/users/logar', async (req, res) => {
+    res.send(await Logar(req.body));
+});
+app.post('/api/users/deslogar', async (req, res) => {
+    res.send(await Deslogar());
+});
 
 //PORT
 app.listen(3030, () => {
