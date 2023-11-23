824e3be4bdb8ccc7a2b448f5cf3ba07baadca11c
Pablo Iago Guaicurus da Silva <104840674+pabloguaicurus@users.noreply.github.com>
2023-11-22 22:14:31 -0300

    inserção de relatório
    VAi dar merdaaa vai dar merdaaa vai dar merdaaaaaa

--- utils/dashboards.py
+++ utils/dashboards.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-#para colocar o dashboard no ar usar o comando: streamlit run dashboards.py
+
+# para colocar o dashboard no ar usar o comando: streamlit run dashboards.py
 
 st.set_page_config(page_title='Dashboard')
 
@@ -11,17 +12,9 @@ with st.container():
 with st.container():
     st.write('Saídas')
     df_out = pd.read_csv('dadosout.csv', sep=';', decimal=',')
-    st.line_chart(df_out, y='Valor R$', x='Data',color='#FF0000')
+    st.line_chart(df_out, y='Valor R$', x='Data', color='#FF0000')
 
 with st.container():
     st.write('Entradas')
     df_in = pd.read_csv('dadosin.csv', sep=';', decimal=',')
-    st.line_chart(df_in, y='Valor R$', x='Data',color='#0000FF')
-
-
-
-
-
-
-
-
+    st.line_chart(df_in, y='Valor R$', x='Data', color='#0000FF')
